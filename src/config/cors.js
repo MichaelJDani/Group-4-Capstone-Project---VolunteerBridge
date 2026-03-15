6d43d4b751b28a2e7d84955336ad2a63139c76b2
Oladele Sunday O. <sundayoladele2@gmail.com>
2026-03-15 22:50:11 +0100

    input the domain name on .env and cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,35 +1,15 @@
-<<<<<<< HEAD
-
-import cors from 'cors';
-import configuration from './env.js';
-=======
 import { configuration } from './env.js';
->>>>>>> main-back-up
 
 export const corsOptions = {
-  origin: ['http://localhost:3000'], // frontend URL
+  origin(origin, callback) {
+    if (!origin || configuration.ALLOWED_ORIGIN.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 };
-
-// export const corsOptions = {
-//     origin(origin, callback) {
-//         if (!origin || configuration.ALLOWED_ORIGIN.includes(origin)) {
-//             return callback(null, true);
-//         }
-
-<<<<<<< HEAD
-//         return callback(new Error("Not allowed by CORS"));
-//     },
-
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"]
-// };
-=======
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-};
->>>>>>> main-back-up
