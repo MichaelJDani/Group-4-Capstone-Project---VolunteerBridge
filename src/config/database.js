db4877dfe17f2cfd5f0a6592d270155bd844df66
Oladele Sunday <sundayoladele2@gmail.com>
2026-03-17 01:04:10 +0100

    mysql change to postgresql

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,18 @@
-import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql'
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432, // default PostgreSQL port
+    dialect: 'postgres',
+    logging: console.log, // optional, remove if you don’t want SQL logs
+  }
 );
 
 export default sequelize;
