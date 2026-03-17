864e338b2419bcb48b91fa2d1d8560d63da013be
Oladele Sunday O. <sundayoladele2@gmail.com>
2026-03-17 14:36:55 +0100

    database.js details corrected

--- src/config/database.js
+++ src/config/database.js
@@ -3,16 +3,16 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432, // default PostgreSQL port
-    dialect: 'postgres',
-    logging: console.log, // optional, remove if you don’t want SQL logs
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // very important for Render
+    },
+  },
+  logging: console.log, // optional: shows SQL queries
+});
 
 export default sequelize;
